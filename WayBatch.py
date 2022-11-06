03162e665e9181a4373b76cedde65f30da947cc2
jylin <jylin1@163.com>
2022-11-06 20:22:03 +0800

    <feat>re.findall between two {{:}}

--- WayBatch.py
+++ WayBatch.py
@@ -11,4 +11,12 @@ def segments_dict(file_path):
 
 
 
-        
+# print(re.match('ydook', 'www.ydook.com').span())  
+# print(re.match('com', 'www.ydook.com'))         
+
+result = re.findall(r'(\w+)=(\d+)', 'set width=20 and height=10')
+print(result)
+
+
+result = re.findall(r'{{\s*(way\S*)\s*:\s*(\S+)\s*}}', 'set width=20 {{way: 123 }} and the {{   way : abas }} height=10')
+print(result)
