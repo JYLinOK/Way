89908f51e12825c237a11cd75b9878fe0d79f7a9
jylin <jylin1@163.com>
2022-11-07 15:55:26 +0800

    <feat>run_waybatch.py test OK!

--- run_waybatch.py
+++ run_waybatch.py
@@ -2,10 +2,9 @@
 author: Jinwei Lin
 time: 2022/11/06
 usage: This is a simple example of using the WayBatch.
-
-
-
+note: If you want to use WayBath in Way editing, set save_dir as ./htmledit/.. 
 """
+
 import WayBatch as wb
 
 
@@ -15,8 +14,8 @@ template_f_path = './batchfiles/template/template.html'
 
 segs_dict = {
     'f1.html':{
-        'way_id_1': 'str_content_1',
-        'way_id_2': 'str_content_2',
+        'way_id_1': './batchfiles/insertfiles/file_content_1.txt',
+        'way_id_2': './batchfiles/insertfiles/file_content_2.html',
     },
     'f2.html':{
         'way_id_1': 'str_content_3',
