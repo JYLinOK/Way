8620b0f17c6601dba1d0ad8587283e8d5d055cb4
jylin <jylin1@163.com>
2022-11-07 15:50:01 +0800

    <update>readme

--- README.md
+++ README.md
@@ -11,13 +11,19 @@
 2. Run WayBatch.py to generate batch file with same template and different data. 
 3. You can run run_waybatch.py to quickly run the WayBatch.
 
-
 ## Note:
-1. If want to use WayBatch, git clone the jtc by following code:
+### 1. If want to use WayBatch, git clone the jtc by following code:
 ```git
 git clone https://github.com/JYLinOK/jtc.git
 ```
 
+### 2. For WayBatch
+1. The default workspace of WayBatch is ./batchfiles.
+2. The ./batchfiles/insertfiles saves the files that will be inserted.
+3. The ./batchfiles/template saves the template files that is only one.
+4. The ./batchfiles/output saves the ouput files that are generate automatically. 
+5. If use the str values to insert, define them in the corresponding code.
+6. If you want to use WayBath in Way editing, set save_dir as ./htmledit/.. 
 
 
 ## UI Show:
--- run_waybatch.py
+++ run_waybatch.py
@@ -3,12 +3,7 @@ author: Jinwei Lin
 time: 2022/11/06
 usage: This is a simple example of using the WayBatch.
 
-Note:
-1. The default workspace of WayBatch is ./batchfiles.
-2. The ./batchfiles/insertfiles saves the files that will be inserted.
-3. The ./batchfiles/template saves the template files that is only one.
-4. The ./batchfiles/output saves the ouput files that are generate automatically. 
-5. If use the str values to insert, define them in the corresponding code.
+
 
 """
 import WayBatch as wb
@@ -16,6 +11,7 @@ import WayBatch as wb
 
 
 save_dir = './batchfiles/ouput/'
+template_f_path = './batchfiles/template/template.html'
 
 segs_dict = {
     'f1.html':{
@@ -26,10 +22,14 @@ segs_dict = {
         'way_id_1': 'str_content_3',
         'way_id_2': 'str_content_4',
     },
+    'f3.html':{
+        'way_id_1': 'str_content_4',
+        'way_id_2': 'str_content_5',
+    },
 }
 
 
-
+wb.batch_files(segs_dict, template_f_path, save_dir)
 
 
 
